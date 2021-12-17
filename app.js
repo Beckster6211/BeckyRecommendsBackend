72edda6660d5506081228884e6e883e5446b325a
= <beckyljones151192@gmail.com>
2021-12-17 16:08:49 +0000

    started on model and routes becky rec food, get all

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
 const port = 3002;
 
 const dotenv = require("dotenv");
 dotenv.config();
 
+const foodRouter = require("./routes/food");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/becfood", foodRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ routes/food.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllFood,
+  //   addFood,
+  //   deleteFood,
+  //   updateFood,
+} = require("../models/food");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await getAllFood();
+    res.json({ success: true, data: result });
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+});
+
+/*
+router.post("/", async (req, res, next) => {
+  try {
+    const data = req.body;
+    console.log({ data });
+    const newFood = await addFood(data);
+    console.log({ newFood });
+    res.json({ success: true, payload: newFood });
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    let delFood = await deleteFood(id);
+    res.json({ success: true, payload: delFood });
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+});
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    let update = req.body;
+    // console.log(update);
+    let updatedFood = await updateFood(id, update);
+    // console.log(updatedFood);
+    res.json({ success: true, payload: updatedFood });
+  } catch (error) {
+    console.log(error);
+  }
+  next();
+});
+*/
+module.exports = router;
