2fdf53035979d83786cc3395ed9baba0043a7bf3
= <beckyljones151192@gmail.com>
2022-01-14 16:52:27 +0000

    added basic commented put none needed routes and models for film, food, read, stay, tele and visit

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const port = 3002;
 // const dotenv = require("dotenv");
 // dotenv.config();
 
+const filmRouter = require("./routes/film");
 const foodRouter = require("./routes/food");
+const readRouter = require("./routes/read");
+const stayRouter = require("./routes/stay");
+const teleRouter = require("./routes/tele");
+const visitRouter = require("./routes/visit");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -15,7 +20,12 @@ app.get("/", (req, res) => {
 app.use(express.json());
 app.use(cors());
 
+app.use("/becfilm", filmRouter);
 app.use("/becfood", foodRouter);
+app.use("/becread", readRouter);
+app.use("/becstay", stayRouter);
+app.use("/bectele", teleRouter);
+app.use("/becvisit", visitRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ routes/film.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllFilm,
+  //   addFilm,
+  //   deleteFilm,
+  //   updateFilm,
+} = require("../models/film");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await getAllFilm();
+    res.json({ success: true, data: result });
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+});
+/*
+router.post("/", async (req, res, next) => {
+  try {
+    const data = req.body;
+    console.log({ data });
+    const newFilm = await addFilm(data);
+    console.log({ newFilm });
+    res.json({ success: true, payload: newFilm });
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    let delFilm = await deleteFilm(id);
+    res.json({ success: true, payload: delFilm });
+  } catch (err) {
+    console.log(err);
+  }
+  next();
+});
+
+router.patch("/:id", async (req, res, next) => {
+  try {
+    // console.log("inside route");
+    let id = req.params.id;
+    let update = req.body;
+    // console.log("patch request");
+    // console.log({ update });
+    let updatedFilm = await updateFilm(id, update);
+    // console.log({ updatedTele });
+    res.json({ success: true, payload: updatedFilm });
+  } catch (error) {
+    console.log(error);
+  }
+  next();
+});
+*/
+module.exports = router;
