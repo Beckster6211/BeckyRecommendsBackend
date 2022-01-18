4fd1c10f257ae0dd364bb6af6c961e25f02d6707
= <beckyljones151192@gmail.com>
2022-01-18 12:28:20 +0000

    added connection string to db index.js

--- db/index.js
+++ db/index.js
@@ -19,6 +19,7 @@ const pool = new Pool({
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
   port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
